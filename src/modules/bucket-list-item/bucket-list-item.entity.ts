8b847a229262829543eb4190c6efe0d44c0bf02c
d5ng <dong5ffice@gmail.com>
2026-02-07 19:49:15 +0900

    feat: add BucketListItem entity and module for managing individual bucket list items

---
+++ src/modules/bucket-list-item/bucket-list-item.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { BucketList } from '../bucket-list/bucket-list.entity'
+import { Destination } from '../destination/destination.entity'
+
+@Entity()
+export class BucketListItem {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @ManyToOne(() => BucketList, (bucketList) => bucketList.items)
+  bucketList: BucketList
+
+  @ManyToOne(() => Destination, (destination) => destination.bucketListItems)
+  destination: Destination
+
+  @Column({ type: 'boolean', default: false })
+  achieved: boolean
+}
