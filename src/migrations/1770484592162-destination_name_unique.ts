ae66852f2529b0668f6a47516a6388146e9e2069
d5ng <dong5ffice@gmail.com>
2026-02-08 02:21:21 +0900

    feat: add migration to enforce unique constraints on destination name and user username

---
+++ src/migrations/1770484592162-destination_name_unique.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class DestinationNameUnique1770484592162 implements MigrationInterface {
+    name = 'DestinationNameUnique1770484592162'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "destination" ADD CONSTRAINT "UQ_8a962921d15e2f4cfa8eba67482" UNIQUE ("name")`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb"`);
+        await queryRunner.query(`ALTER TABLE "destination" DROP CONSTRAINT "UQ_8a962921d15e2f4cfa8eba67482"`);
+    }
+
+}
