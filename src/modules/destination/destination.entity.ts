1ef81c121bb121e1b543de1ac969ec5d37b025ee
d5ng <dong5ffice@gmail.com>
2026-02-07 19:49:19 +0900

    feat: add Destination entity and module for managing travel destinations

---
+++ src/modules/destination/destination.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { BucketListItem } from '../bucket-list-item/bucket-list-item.entity'
+
+@Entity()
+export class Destination {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  name: string
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description?: string
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  location: string
+
+  @OneToMany(
+    () => BucketListItem,
+    (bucketListItem) => bucketListItem.destination,
+  )
+  bucketListItems: BucketListItem[]
+}
