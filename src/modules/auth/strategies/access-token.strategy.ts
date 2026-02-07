820a03639aee286c7ab2fa94955812d295c88086
d5ng <dong5ffice@gmail.com>
2026-02-07 22:08:41 +0900

    feat: implement AccessTokenStrategy for JWT access token validation

---
+++ src/modules/auth/strategies/access-token.strategy.ts
@@ -0,0 +1,19 @@
+import { JwtPayload } from '@/shared/types/jwt.type'
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+
+@Injectable()
+export class AccessTokenStrategy extends PassportStrategy(Strategy, 'jwt-access-token') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_ACCESS_SECRET')!,
+    })
+  }
+
+  validate(payload: JwtPayload): JwtPayload {
+    return payload
+  }
+}
