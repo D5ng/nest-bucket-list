553043f2aefa2251dca9d6f8beef43b4639affcf
d5ng <dong5ffice@gmail.com>
2026-02-07 17:57:02 +0900

    feat: integrate ConfigModule and TypeOrmModule for database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import typeormConfig from './config/typeorm.config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const config = configService.get<TypeOrmModuleOptions>('typeorm')
+
+        if (!config) {
+          throw new Error('TypeORM 설정을 찾을 수 없어요.')
+        }
+
+        return config
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
