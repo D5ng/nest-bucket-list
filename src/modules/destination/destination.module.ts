46fa28ff2f0a7dbf0ec172724ab28f4e978c52e6
d5ng <dong5ffice@gmail.com>
2026-02-08 02:21:16 +0900

    feat: configure DestinationsModule with TypeOrm integration, including DestinationService and DestinationController

--- src/modules/destination/destination.module.ts
+++ src/modules/destination/destination.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DestinationController } from './destination.controller'
+import { Destination } from './destination.entity'
+import { DestinationService } from './destination.service'
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Destination])],
+  providers: [DestinationService],
+  controllers: [DestinationController],
+})
 export class DestinationsModule {}
