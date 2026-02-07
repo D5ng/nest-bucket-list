bbea63648241ff209f41b59b03eba7530abd4501
d5ng <dong5ffice@gmail.com>
2026-02-08 01:38:24 +0900

    refactor: rename user profile methods in UserController for clarity and remove unused deleteUser method

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { AccessTokenGuard } from '@/shared/guards/access-token.guard'
-import { Body, Controller, Delete, Get, Put, Req, UnauthorizedException, UseGuards } from '@nestjs/common'
+import { Body, Controller, Get, Put, Req, UnauthorizedException, UseGuards } from '@nestjs/common'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { User } from './user.entity'
 import { UserService } from './user.service'
@@ -10,7 +10,7 @@ export class UserController {
 
   @UseGuards(AccessTokenGuard)
   @Get('profile')
-  async getMe(@Req() req: RequestWithUser) {
+  async getProfile(@Req() req: RequestWithUser) {
     const userId = req.user['sub']
 
     if (!userId) {
@@ -28,7 +28,7 @@ export class UserController {
 
   @UseGuards(AccessTokenGuard)
   @Put('profile')
-  async updateUser(@Req() req: RequestWithUser, @Body() updateUserDto: UpdateUserDto) {
+  async updateProfile(@Req() req: RequestWithUser, @Body() updateUserDto: UpdateUserDto) {
     const userId = req.user['sub']
 
     if (!userId) {
@@ -38,18 +38,6 @@ export class UserController {
     const user = await this.userService.update(userId, updateUserDto)
 
     return this.shieldUserInfo(user)
-  }
-
-  @UseGuards(AccessTokenGuard)
-  @Delete('profile')
-  async deleteUser(@Req() req: RequestWithUser) {
-    const userId = req.user['sub']
-
-    if (!userId) {
-      throw new UnauthorizedException('접근 권한이 없습니다.')
-    }
-
-    return this.userService.remove(userId)
   }
 
   private shieldUserInfo({ id, name, username }: User) {
