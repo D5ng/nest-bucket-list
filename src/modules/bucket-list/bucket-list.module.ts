2cdfaa54f5594bf60d108cfe84bcb0ece04eff5a
d5ng <dong5ffice@gmail.com>
2026-02-08 16:52:45 +0900

    feat: configure BucketListModule with TypeOrm integration, adding BucketListService and BucketListController

--- src/modules/bucket-list/bucket-list.module.ts
+++ src/modules/bucket-list/bucket-list.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../user/user.entity'
+import { BucketListController } from './bucket-list.controller'
+import { BucketList } from './bucket-list.entity'
+import { BucketListService } from './bucket-list.service'
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([BucketList, User])],
+  providers: [BucketListService],
+  controllers: [BucketListController],
+})
 export class BucketListModule {}
