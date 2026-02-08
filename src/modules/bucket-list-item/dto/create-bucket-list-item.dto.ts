2af882032a1d6d4b73d1fbfccf77ec273edd1185
d5ng <dong5ffice@gmail.com>
2026-02-08 21:44:27 +0900

    feat: add validation to CreateBucketListItemDto for destinationId to ensure it is a required number

--- src/modules/bucket-list-item/dto/create-bucket-list-item.dto.ts
+++ src/modules/bucket-list-item/dto/create-bucket-list-item.dto.ts
@@ -1,3 +1,7 @@
+import { IsNotEmpty, IsNumber } from 'class-validator'
+
 export class CreateBucketListItemDto {
+  @IsNumber()
+  @IsNotEmpty({ message: '여행지 ID는 필수 입력 항목입니다.' })
   destinationId: number
 }
